fee094598f37f902085252e78733620dd3822af2
adityatriand <adityatriananda0@gmail.com>
2021-10-21 22:26:16 +0700

    change name user in navbar

--- application/views/admin/header/header.php
+++ application/views/admin/header/header.php
@@ -217,7 +217,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="<?= base_url('template/') ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs"><?= $nama ?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
